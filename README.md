9d829464ee393c33d7b1328babc6b999a471a8cc
Kevin Hogan <kchogan@pacbell.net>
2024-01-19 16:56:31 -0800

    Now includes additional requirements

--- README.md
+++ README.md
@@ -1,23 +1,50 @@
 
-  # readme-generator
+# ReadMe Generator
 
-  ## Description
-  This is a node.js application that prompts for project information and automatically generates a high-quality README.md file using that information.  
-    
-  ## Installation
-  
-  After cloning the repo, please run 'npm install' to get the dependent modules (e.g. inquirer).
-  
-  ## Usage
-  
-  After the modules are installed, run by typing 'node index.js' from the terminal.  You will then be prompted with a series of project related questions.
-  
-  ![alt ReadMe Generator Screenshot](./assets/capture.png)
-  
-  ## Credits
-  
-  N/A
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-  ## License
-  
-  Please refer to the LICENSE in the repo.
+## Description
+This is a node.js application that prompts for project information and automatically generates a high-quality README.md file using that information.  
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [How to Contribute](#guidelines)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Installation
+
+After cloning the repo, please run 'npm install' to get the dependent modules (e.g. inquirer).
+
+## Usage
+
+After the modules are installed, run by typing 'node index.js' from the terminal.  You will then be prompted with a series of project related questions.
+
+![alt ReadMe generator screenshot](./assets/capture.png)
+
+## Credits
+
+N/A
+
+## License
+
+This application is covered under the MIT License.
+
+<a id="guidelines"></a>
+## How To Contribute
+
+If you choose to contribute, please refer to the [Contributor Covenant](https://www.contributor-covenant.org/) for guidelines.
+
+## Tests
+
+N/A
+
+## Questions
+
+[GitHub Profile](https://github.com/kevinchogan)
+
+For questions, please contact kchogan@pacbell.net.
--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const returnBadge = (license) => {
+  let badge;
+  switch (license) {
+    case "Apache License 2.0":
+      badge =
+        "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "GNU General Public License v3.0":
+      badge =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "MIT License":
+      badge =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "BSD 2-Clause 'Simplified' License":
+      badge =
+        "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+      break;
+    case "BSD 3-Clause 'New' or 'Revised' License":
+      badge =
+        "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+      break;
+    case "Boost Software License 1.0":
+      badge =
+        "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+      break;
+    case "Creative Commons Zero v1.0 Universal":
+      badge =
+        "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
+      break;
+    case "Eclipse Public License 2.0":
+      badge =
+        "[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/licenses/EPL-2.0)";
+      break;
+  }
+  return badge;
+};
+
 /* === promptUser ===
 Prompts the user with a series of questions used to populate the readme file.
 After which it writes the file.
@@ -44,6 +83,42 @@ const promptUser = () => {
           "Please provide a list of credits, including collaborators, 3rd party assets, or tutorials used in the making of this project.",
         name: "credits",
       },
+      {
+        type: "list",
+        message: "Please select a license agreement.",
+        choices: [
+          "Apache License 2.0",
+          "GNU General Public License v3.0",
+          "MIT License",
+          "BSD 2-Clause 'Simplified' License",
+          "BSD 3-Clause 'New' or 'Revised' License",
+          "Boost Software License 1.0",
+          "Creative Commons Zero v1.0 Universal",
+          "Eclipse Public License 2.0",
+        ],
+        name: "license",
+      },
+      {
+        type: "input",
+        message: "Please provide contribution guidelines.",
+        name: "guidelines",
+      },
+      {
+        type: "input",
+        message: "Please provide testing instructions.",
+        name: "tests",
+      },
+      {
+        type: "input",
+        message: "What is your GitHub handle?",
+        name: "gitHub",
+      },
+      {
+        type: "input",
+        message:
+          "Please provide your email address for people to send questions to.",
+        name: "email",
+      },
     ])
     .then((answers) => {
       // generates readme markdown based on answers
@@ -66,31 +141,66 @@ const makeReadMe = ({
   scAlt,
   scPath,
   credits,
-}) =>
-  `
-  # ${title}
+  license,
+  guidelines,
+  tests,
+  gitHub,
+  email,
+}) => {
+  let badge = returnBadge(license);
+  let mdText = `
+# ${title}
 
-  ## Description
-  ${description}  
-    
-  ## Installation
-  
-  ${installation}
-  
-  ## Usage
-  
-  ${usage}
-  
-  ![alt ${scAlt}](${scPath})
-  
-  ## Credits
-  
-  ${credits}
+${badge}
 
-  ## License
-  
-  Please refer to the LICENSE in the repo.
+## Description
+${description}  
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [How to Contribute](#guidelines)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+![alt ${scAlt}](${scPath})
+
+## Credits
+
+${credits}
+
+## License
+
+This application is covered under the ${license}.
+
+<a id="guidelines"></a>
+## How To Contribute
+
+${guidelines}
+
+## Tests
+
+${tests}
+
+## Questions
+
+[GitHub Profile](https://github.com/${gitHub})
+
+For questions, please contact ${email}.
 `;
+  return mdText;
+};
 
 /* === Main ===
 Runs the function used to prompt the user with a series of project-related questions.
